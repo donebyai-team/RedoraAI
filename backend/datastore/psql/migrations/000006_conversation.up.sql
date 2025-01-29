6b3632946f2c645c1e922337621ec9483a46704d
Shashank Agarwal <shank.agarwal318@gmail.com>
2025-01-29 13:13:50 +0530

    Added sessions

--- backend/datastore/psql/migrations/000006_conversation.up.sql
+++ backend/datastore/psql/migrations/000006_conversation.up.sql
@@ -3,19 +3,18 @@ BEGIN;
 CREATE TABLE conversations
 (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
-    customer_id uuid NOT NULL,
+    session_id uuid NOT NULL,
     from_phone varchar(255) NOT NULL,
-    organization_id uuid NOT NULL,
-    status varchar(255) NOT NULL,
+    status character varying(255) NOT NULL DEFAULT 'QUEUED',
     summary TEXT DEFAULT '',
+    voice_provider character varying(255) NOT NULL,
     call_duration integer DEFAULT 0,
+    recording_url TEXT,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  timestamp
 );
 
-ALTER TABLE conversations ADD CONSTRAINT fk1_conversations FOREIGN KEY (organization_id) REFERENCES organizations (id);
-ALTER TABLE conversations ADD CONSTRAINT fk2_conversations FOREIGN KEY (customer_id) REFERENCES customers (id);
-
+ALTER TABLE conversations ADD CONSTRAINT fk1_conversations FOREIGN KEY (session_id) REFERENCES customer_sessions (id);
 
 CREATE TRIGGER trigger_record_changed_on_conversations
     BEFORE UPDATE
---
+++ backend/datastore/psql/migrations/000007_session.up.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+CREATE TABLE customer_sessions
+(
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    phone varchar(255) NOT NULL,
+    customer_id uuid NOT NULL,
+    organization_id uuid NOT NULL,
+    external_id varchar(255),
+    due_date  timestamp NOT NULL,
+    status character varying(255) NOT NULL DEFAULT 'QUEUED',
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  timestamp
+);
+
+ALTER TABLE customer_sessions ADD CONSTRAINT fk1_customer_sessions FOREIGN KEY (customer_id) REFERENCES customers (id);
+ALTER TABLE customer_sessions ADD CONSTRAINT fk2_customer_sessions FOREIGN KEY (organization_id) REFERENCES organizations (id);
+
+CREATE TRIGGER trigger_record_changed_on_customer_sessions
+    BEFORE UPDATE
+    ON customer_sessions
+    FOR EACH ROW
+    EXECUTE PROCEDURE record_changed();
+
+COMMIT;
