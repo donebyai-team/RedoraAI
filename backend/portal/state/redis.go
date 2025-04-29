ad5dde6e62403e85e6ec2b7588e90a12e4a98b60
Shashank Agarwal <shank.agarwal318@gmail.com>
2025-04-29 16:38:41 +0530

    added redis

--- backend/portal/state/redis.go
+++ backend/portal/state/redis.go
@@ -3,6 +3,8 @@ package state
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/url"
 	"strings"
 	"time"
 
@@ -18,7 +20,39 @@ type redisStore struct {
 }
 
 func NewRedisStore(redisAddr string, logger *zap.Logger) *redisStore {
-	redisClient := redis.NewClient(&redis.Options{Addr: redisAddr})
+	var redisClient *redis.Client
+	// Check if redisAddr starts with the redis:// scheme
+	if len(redisAddr) > 6 && redisAddr[:6] == "redis:" {
+		// Parse the Redis URL
+		parsedURL, err := url.Parse(redisAddr)
+		if err != nil {
+			log.Fatalf("Error parsing Redis URL: %v", err)
+		}
+
+		// Extracting user and password from the URL
+		password, _ := parsedURL.User.Password()
+
+		// Extracting the host and port
+		host := parsedURL.Hostname()
+		port := parsedURL.Port()
+
+		// Set up Redis client options
+		options := &redis.Options{
+			Addr:     fmt.Sprintf("%s:%s", host, port),
+			Password: password, // Password from the URL
+		}
+		redisClient = redis.NewClient(options)
+	} else {
+		// Use the simple address like localhost:6379
+		redisClient = redis.NewClient(&redis.Options{
+			Addr: redisAddr,
+		})
+	}
+
+	_, err := redisClient.Ping().Result()
+	if err != nil {
+		logger.Error("Error connecting to Redis", zap.Error(err))
+	}
 	return &redisStore{
 		redisClient: redisClient,
 		logger:      logger,
