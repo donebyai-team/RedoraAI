9f7575a813cea04ccc3041df8bb1456304e65219
Shashank Agarwal <shank.agarwal318@gmail.com>
2025-05-09 14:09:20 +0530

    fixed rate limiting

--- backend/ai/relevancy_post_test.go
+++ backend/ai/relevancy_post_test.go
@@ -17,7 +17,7 @@ func init() {
 	logging.InstantiateLoggers(logging.WithDefaultLevel(zap.DebugLevel))
 }
 
-func TestRelevancyOutputFormating(t *testing.T) {
+func TestRelevancyOutputFormating2(t *testing.T) {
 	debugStore, err := dstore.NewStore("../../data/debugstore", "", "", false)
 	if err != nil {
 		t.FailNow()
@@ -41,6 +41,45 @@ func TestRelevancyOutputFormating(t *testing.T) {
 		Author:      "Feeling_Ad_4458",
 		Title:       utils.Ptr("Got fired. Trying to build a SaaS that actually helps small service providers stand out. Stuck on the value part — would love your thoughts"),
 		Description: "Hey everyone, I recently got fired and decided it’s time to take a real shot at independence. I live in a country of about 7 million people. It’s not a huge market, but I believe people are willing to pay for something that speaks their language , literally and culturally.\n\nOver here, there are tons of fitness trainers, yoga and pilates instructors, NLP coaches, private tutors, emotional therapists, and other solo service providers. Most of them are active on Instagram or TikTok. They post reels, stories, run ads, hire digital marketers. And honestly? It’s all starting to look the same.\n\nSame voiceovers. Same captions. Same generic editing. When I’m looking for a service myself, I hate being sold to like that. I don’t want a slogan or a hype video — I want to get a real feel for the person. I want to hear them talk, see how they think.\n\nThat led me to an idea.\n\nWhat if I gave these professionals a way to show who they really are not through another ad but through a mini-course generator. Something simple and beautiful. They upload a video or two, write a few lines about their method, maybe add a short quiz — and boom, it becomes a personal landing page they can link to from their bio. The idea is to help them stand out, explain their approach better than a 15-second reel ever could, and win trust.\n\nSo I started working on this.\n\nThe platform generates a beautiful little course, like a teaser that helps the potential client understand the trainer’s mindset, style, or process. For example, a fitness coach might share videos on how to train arms, and one video about motivation and how he gets clients to stay consistent.\n\nSounds great, right?\n\nHere’s where I got stuck.\n\nThat coach now has a great-looking link they can share. People might watch, get value, and even reach out. But then… that’s it. They don’t need to create another mini-course. They just needed a nice way to present themselves once. Why would they pay monthly for that?\n\nI started realizing that the coach doesn’t want a platform — they want more clients. So now I’m at a crossroads. Do I pivot into something that helps generate leads, not just present better? Or is there a way to build ongoing value around that mini-course idea?\n\nI still love the concept of helping service providers differentiate themselves through deeper, more honest content. But I’m not sure how to turn that into something they’ll happily pay for every month.\n\nWould really appreciate any insights, directions, or even examples of tools that are doing something similar.\n\nThanks for reading.",
+	}
+
+	org := &models.Organization{}
+
+	relevant, usage, errRel := ai.IsRedditPostRelevant(context.Background(), org, project, post, logger)
+	if errRel != nil {
+		t.FailNow()
+	}
+
+	comment := utils.FormatComment(relevant.SuggestedComment)
+	assert.NotEmpty(t, comment)
+	assert.True(t, relevant.IsRelevantConfidenceScore >= 90)
+	assert.Equal(t, usage.Model, defaultModel)
+}
+
+func TestRelevancyOutputFormating(t *testing.T) {
+	debugStore, err := dstore.NewStore("../../data/debugstore", "", "", false)
+	if err != nil {
+		t.FailNow()
+	}
+	defaultModel := models.LLMModel("redora-dev-gpt-4.1-2025-04-14")
+	//defaultModel := models.LLMModel("redora-gemini-2.0-flash")
+	ai, err := NewOpenAI(utils.GetEnvTestReq(t, "OPENAI_API_KEY_DEV"), defaultModel, LangsmithConfig{}, debugStore, logger)
+	if err != nil {
+		t.FailNow()
+	}
+
+	project := &models.Project{
+		ID:                 "XXX",
+		OrganizationID:     "XXXXX",
+		Name:               "MiraaAI",
+		ProductDescription: "Miraa helps B2B SaaS businesses generate high quality leads and organic growth via Content-led SEO and GEO. We offers services like Content, SEO, and Reddit lead generation",
+		CustomerPersona:    "Founders, CMOs, CEOs, and Head of marketing/growth",
+	}
+
+	post := &models.Lead{
+		Author:      "Feeling_Ad_4458",
+		Title:       utils.Ptr("Query Alert: Need some help here - Any content SEO strategies to rank in another country? -\n"),
+		Description: "Say I am sitting in KL (Malaysia) and want my content to rank in the NY and New Jersey area (US). Looking for some grey hat ideas.",
 	}
 
 	org := &models.Organization{}
