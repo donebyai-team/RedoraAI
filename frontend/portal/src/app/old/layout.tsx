ddf42f8a63d70660b19fe98e075b13869062f1e5
Ajay Vaghela <ajayvaghela362560@gmail.com>
2025-06-05 14:21:58 +0530

    remove old code

--- frontend/portal/src/app/old/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PortalAuthProvider } from '../../provider/PortalAuthProvider'
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (<PortalAuthProvider>{children}</PortalAuthProvider>)
-}
