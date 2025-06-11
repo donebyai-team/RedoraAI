9d205cdc4bd81db33a4c9a5313aafc6eb2380844
Ajay Vaghela <ajayvaghela362560@gmail.com>
2025-06-11 12:00:11 +0530

    implement new billing page ui

--- frontend/portal/src/app/(restricted)/(pages)/settings/billing/page.tsx
+++ frontend/portal/src/app/(restricted)/(pages)/settings/billing/page.tsx
@@ -1,23 +1,5 @@
-'use client'
-
-import { Card, Paper, CardContent, Typography } from "@mui/material";
-import { Box } from '@mui/system'
+import Billing from '@/components/pages/Billing'
 
 export default function Page() {
-
-    return (<>
-        <Box component="main" sx={{ flexGrow: 1, p: 0, display: "flex", flexDirection: "column" }}>
-
-            <Box sx={{ p: 3, flexGrow: 1 }}>
-                <Card sx={{ p: 4, mt: 5 }} component={Paper}>
-                    <CardContent>
-
-                        <Typography variant="h6" color="text.secondary" sx={{ mb: 4 }}>
-                            Connect via chat to UPGRADE
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Box>
-        </Box>
-    </>);
+  return (<Billing />);
 }
