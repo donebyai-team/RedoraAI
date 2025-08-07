67237686bf0829b31a2c7f23ad06e1c85fe8dad6
Shashank Agarwal <shank.agarwal318@gmail.com>
2025-08-07 18:27:11 +0530

    logged user agent

--- backend/integrations/reddit/oauth.go
+++ backend/integrations/reddit/oauth.go
@@ -37,7 +37,7 @@ func (u *userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error
 	if u.userName != "" {
 		req.Header.Set("User-Agent", fmt.Sprintf("com.redoraai:v0.1 by (/u/%s)", u.userName))
 	} else {
-		req.Header.Set("User-Agent", "com.redoraai:v0.1 by (redora)")
+		req.Header.Set("User-Agent", "com.redoraai:v0.1.0 by (redora)")
 	}
 	return u.base.RoundTrip(req)
 }
